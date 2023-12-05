11c2694f1d00d0350d309744787b57c18d3ad836
Allen Myers <am@zest.ai>
2023-12-05 10:21:18 -0800

    chore: replace rafamadriz/friendly-snippets with armyers/friendly-snippets until rafamadriz/friendly-snippets includes terraform by default

--- lua/plugins/luasnip.lua
+++ lua/plugins/luasnip.lua
@@ -2,6 +2,19 @@ return {
   "L3MON4D3/LuaSnip",
   dependencies = {
     {
+      -- HACK: disable this one since it doesn't include terraform in package.json
+      "rafamadriz/friendly-snippets",
+      enabled = false,
+    },
+    {
+      -- HACK: just a fork of rafamadriz/friendly-snippets with terraform language enabled
+      "armyers/friendly-snippets",
+      enabled = true,
+      config = function()
+        require("luasnip.loaders.from_vscode").lazy_load()
+      end,
+    },
+    {
       "Katlean/local-snippets",
       url = "git@github.com:Katlean/local-snippets.git",
       config = function()
