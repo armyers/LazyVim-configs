44306c5466ef136052920b4451cda25f2530c7c6
Allen Myers <am@zest.ai>
2024-01-15 09:42:23 -0800

    feat(mini-splitjoin): add so I can play with it

---
+++ lua/plugins/mini-splitjoin.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    "echasnovski/mini.splitjoin",
+    event = "VeryLazy",
+    version = false,
+    config = function()
+      require("mini.splitjoin").setup({})
+    end,
+  },
+}
