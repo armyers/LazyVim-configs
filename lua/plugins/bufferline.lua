55b27603f5b3a248cfd63d8b4120dfd6a2e4175c
Allen Myers <am@zest.ai>
2024-11-17 10:03:18 -0800

    feat: add bufferline plugin and configure to background highlight on active buffer

---
+++ lua/plugins/bufferline.lua
@@ -0,0 +1,20 @@
+return {
+  {
+    "akinsho/bufferline.nvim",
+    event = "VeryLazy",
+    opts = {
+      highlights = {
+        buffer_selected = {
+          bg = "#444444",
+          bold = true,
+          italic = true,
+        },
+      },
+      options = {
+        indicator = {
+          style = "none",
+        },
+      },
+    },
+  },
+}
