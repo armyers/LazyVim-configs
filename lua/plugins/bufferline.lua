49a3a777fac4b799aa3cc533d75fea2a4725ab2b
Allen Myers <am@zest.ai>
2024-11-18 08:18:02 -0800

    chore(bufferline): set bg = #334455 for *_selected highlights

--- lua/plugins/bufferline.lua
+++ lua/plugins/bufferline.lua
@@ -4,10 +4,44 @@ return {
     event = "VeryLazy",
     opts = {
       highlights = {
+        tab_selected = {
+          bg = "#334455",
+        },
+        tab_separator_selected = {
+          bg = "#334455",
+        },
+        close_button_selected = {
+          bg = "#334455",
+        },
         buffer_selected = {
-          bg = "#444444",
-          bold = true,
-          italic = true,
+          bg = "#334455",
+        },
+        modified_selected = {
+          bg = "#334455",
+        },
+        numbers_selected = {
+          bg = "#334455",
+        },
+        diagnostic_selected = {
+          bg = "#334455",
+        },
+        hint_diagnostic_selected = {
+          bg = "#334455",
+        },
+        info_diagnostic_selected = {
+          bg = "#334455",
+        },
+        warning_selected = {
+          bg = "#334455",
+        },
+        warning_diagnostic_selected = {
+          bg = "#334455",
+        },
+        error_selected = {
+          bg = "#334455",
+        },
+        error_diagnostic_selected = {
+          bg = "#334455",
         },
       },
       options = {
